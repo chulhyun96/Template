7c655da5462de1c3183a996849358d3c4227851d
chulhyun96 <park82602@gmail.com>
2025-04-28 05:30:31 +0900

    feat : Post Entity 클래스 정의 및 생성

---
+++ src/main/java/com/example/demo/post/entity/Post.java
@@ -0,0 +1,24 @@
+package com.example.demo.post.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+}

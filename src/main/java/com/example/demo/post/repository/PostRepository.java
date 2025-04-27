ef3d5b06d759ce7daf1318c38e942996e320dced
chulhyun96 <park82602@gmail.com>
2025-04-28 05:32:33 +0900

    feat : PostRepository 추상 메서드 정의(save, findById, findAll)

--- src/main/java/com/example/demo/post/repository/PostRepository.java
+++ src/main/java/com/example/demo/post/repository/PostRepository.java
@@ -1,4 +1,14 @@
 package com.example.demo.post.repository;
 
+import com.example.demo.post.entity.Post;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository {
+    Post save(Post post);
+
+    Optional<Post> findById(Long id);
+
+    List<Post> findAll();
 }

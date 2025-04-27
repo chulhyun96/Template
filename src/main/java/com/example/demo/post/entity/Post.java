9860e610f365bfce6770e8f55b96b95d58aea1af
chulhyun96 <park82602@gmail.com>
2025-04-28 06:03:38 +0900

    feat : Post 생성자 메서드 정의

--- src/main/java/com/example/demo/post/entity/Post.java
+++ src/main/java/com/example/demo/post/entity/Post.java
@@ -1,5 +1,7 @@
 package com.example.demo.post.entity;
 
+import com.example.demo.post.dto.request.PostCreateRequest;
+import com.example.demo.post.dto.response.PostCreateResponse;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,4 +23,13 @@ public class Post {
     private String title;
     private String content;
     private LocalDateTime createdAt;
+
+    public static Post from(PostCreateRequest request, LocalDateTime now) {
+        return Post.builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .createdAt(now)
+                .build();
+
+    }
 }

638715ead569c88478903bc292e88c7243f955e8
chulhyun96 <park82602@gmail.com>
2025-04-28 05:59:02 +0900

    feat : 게시글 생성(save) 메서드 구현

--- src/main/java/com/example/demo/post/controller/PostController.java
+++ src/main/java/com/example/demo/post/controller/PostController.java
@@ -1,11 +1,25 @@
 package com.example.demo.post.controller;
 
+import com.example.demo.post.dto.request.PostCreateRequest;
+import com.example.demo.post.dto.response.PostCreateResponse;
 import com.example.demo.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
+
+    @PostMapping("/posts")
+    public ResponseEntity<PostCreateResponse> createPost(@RequestBody @Validated PostCreateRequest request) {
+        PostCreateResponse response = postService.save(request, LocalDateTime.now());
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/example/demo/post/service/PostService.java
+++ src/main/java/com/example/demo/post/service/PostService.java
@@ -1,11 +1,21 @@
 package com.example.demo.post.service;
 
+import com.example.demo.post.dto.request.PostCreateRequest;
+import com.example.demo.post.dto.response.PostCreateResponse;
+import com.example.demo.post.entity.Post;
 import com.example.demo.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository repository;
+
+    public PostCreateResponse save(PostCreateRequest request, LocalDateTime now) {
+        Post savedPost = repository.save(Post.from(request, now));
+        return savedPost.toModel();
+    }
 }

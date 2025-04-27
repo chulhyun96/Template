e7b516c4dd98527f3b49b3bd89e52684db7991f1
chulhyun96 <park82602@gmail.com>
2025-04-28 06:39:04 +0900

    feat : 단건 조회 및 전체 조회 추가

--- src/main/java/com/example/demo/post/service/PostService.java
+++ src/main/java/com/example/demo/post/service/PostService.java
@@ -2,12 +2,16 @@ package com.example.demo.post.service;
 
 import com.example.demo.post.dto.request.PostCreateRequest;
 import com.example.demo.post.dto.response.PostCreateResponse;
+import com.example.demo.post.dto.response.PostResponse;
 import com.example.demo.post.entity.Post;
+import com.example.demo.post.exception.PostException;
 import com.example.demo.post.repository.PostRepository;
+import com.example.demo.post.type.PostErrorStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +21,17 @@ public class PostService {
     public PostCreateResponse save(PostCreateRequest request, LocalDateTime now) {
         Post savedPost = repository.save(Post.from(request, now));
         return PostCreateResponse.from(savedPost);
+    }
+
+    public PostResponse getById(Long id) {
+        Post findedPost = repository.findById(id)
+                .orElseThrow(() -> new PostException(PostErrorStatus.NOT_FOUND));
+        return PostResponse.from(findedPost);
+    }
+
+    public List<PostResponse> getAll() {
+        return repository.findAll().stream()
+                .map(PostResponse::from)
+                .toList();
     }
 }

0afd1bc85709f8ba911979d936ad029893cb181d
chulhyun96 <park82602@gmail.com>
2025-04-28 05:36:40 +0900

    feat : 메서드 구현 및 트랜잭션 적용

--- src/main/java/com/example/demo/post/repository/PostRepositoryImpl.java
+++ src/main/java/com/example/demo/post/repository/PostRepositoryImpl.java
@@ -3,27 +3,30 @@ package com.example.demo.post.repository;
 import com.example.demo.post.entity.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostRepositoryImpl implements PostRepository{
     private final SpringDataJpaPostRepository repository;
 
     @Override
+    @Transactional
     public Post save(Post post) {
-        return null;
+        return repository.save(post);
     }
 
     @Override
     public Optional<Post> findById(Long id) {
-        return Optional.empty();
+        return repository.findById(id);
     }
 
     @Override
     public List<Post> findAll() {
-        return List.of();
+        return repository.findAll();
     }
 }

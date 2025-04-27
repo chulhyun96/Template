e04a84392ea2bea5ae1a5734e7614edd8bba2315
chulhyun96 <park82602@gmail.com>
2025-04-28 06:30:08 +0900

    feat : 에러 메시지를 JSON으로 일관성있게 하기 위해 ErrorResponse 클래스 생성

---
+++ src/main/java/com/example/demo/app/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.example.demo.app.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private int errorCode;
+    private String errorMessage;
+}

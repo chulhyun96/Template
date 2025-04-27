de0f52e95c1066259212455c40458bc55446225d
chulhyun96 <park82602@gmail.com>
2025-04-28 06:15:48 +0900

    feat : 게시글 단건 조회 응답 DTO 생성

---
+++ src/main/java/com/example/demo/post/dto/response/PostResponse.java
@@ -0,0 +1,23 @@
+package com.example.demo.post.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "게시글 단건 조회 시 응답 DTO")
+public class PostResponse {
+    @Schema(description = "게시글 제목")
+    private String title;
+
+    @Schema(description = "게시글 내용")
+    private String content;
+
+    @Schema(description = "게시글 생성 날짜")
+    private LocalDateTime createdAt;
+}

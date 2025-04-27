a3267fcd238d21a4d63787ca976a0c5f419e40a7
chulhyun96 <park82602@gmail.com>
2025-04-28 05:49:45 +0900

    feat : 게시글 생성 응답 DTO 클래스 생성

---
+++ src/main/java/com/example/demo/post/dto/response/PostCreateResponse.java
@@ -0,0 +1,25 @@
+package com.example.demo.post.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "게시글 생성 응답 DTO 클래스")
+public class PostCreateResponse {
+    @Schema(description = "게시글 제목")
+    private String title;
+
+    @Schema(description = "게시글 내용")
+    private String content;
+
+    @Schema(description = "게시글 생성 날짜")
+    private LocalDateTime createdAt;
+}
